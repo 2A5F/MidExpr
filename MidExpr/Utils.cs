8ca236ac6649bb69a32ed353619a81feb0c2354b
U2A5F <U2A5F@outlook.com>
2025-09-28 23:57:21 +0800

    try register machine

--- MidExpr/Utils.cs
+++ MidExpr/Utils.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 
 namespace MidExpr;
 
