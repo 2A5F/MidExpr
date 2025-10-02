48d1f32d45fe0af32c82fa929a39853abfe325fc
U2A5F <U2A5F@outlook.com>
2025-10-02 22:36:21 +0800

    save

---
+++ .idea/fileTemplates/internal/C Header File.h
@@ -0,0 +1,5 @@
+#pragma once
+#[[#ifndef]] #COPLT_COM_${INCLUDE_GUARD }
+#[[#define]] #COPLT_COM_${INCLUDE_GUARD }
+
+#[[#endif]] #//COPLT_COM_${INCLUDE_GUARD}
---
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,13 @@
+#pragma once
+#[[#ifndef]]# COPLT_COM_${INCLUDE_GUARD}
+#[[#define]]# COPLT_COM_${INCLUDE_GUARD}
+
+${NAMESPACES_OPEN}
+
+class ${NAME} {
+
+};
+
+${NAMESPACES_CLOSE}
+
+#[[#endif]]# //COPLT_COM_${INCLUDE_GUARD}
